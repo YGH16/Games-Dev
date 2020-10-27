92272cc3e6dcb3876a3477cef7f9406b125e5242
YoungHotBrown <sobhanakhtar16@gmail.com>
2020-10-27 13:55:03 +0000

    Player moving
    Player Moving and camera following

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Rigidbody2D rb2d;
+	public float speed;
+	
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		
+		rb2d.AddForce(movement * speed);
+		
+		
+	}
 }
