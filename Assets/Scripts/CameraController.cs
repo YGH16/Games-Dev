32a31789a86bb9fa9a639769e81797bc86954912
YoungHotBrown <sobhanakhtar16@gmail.com>
2020-10-27 14:16:30 +0000

    Camera Script and Made player Movement less floaty
    Increase drag of player resulting in less floaty movement
    Added a camera script to follow the player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	 public GameObject player;
+	 
+	 private Vector3 offset;
+	 
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Called After Each frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
