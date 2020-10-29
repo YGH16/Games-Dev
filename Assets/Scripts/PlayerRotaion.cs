c9f00eab81552d3d9c55558094a91d4685dfe8f7
YoungHotBrown <sobhanakhtar16@gmail.com>
2020-10-29 13:59:44 +0000

    Added PlayerRotation Script
    Player rotates in the direction of the mouse

---
+++ Assets/Scripts/PlayerRotaion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotaion : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+		var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
