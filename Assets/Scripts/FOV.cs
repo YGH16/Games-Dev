0ba20dd813d2027a78c922b10d9dfa1637b605f5
Hagras07 <karimhhh2012@gmail.com>
2020-11-01 08:27:55 -0800

    Added Laser, Layout Changes

---
+++ Assets/Scripts/FOV.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class FOV : MonoBehaviour
+{
+   
+    private void Start()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        float fov = 90f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 50;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 50f;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for(int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            vertices[vertexIndex] = vertex;
+
+            if(i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+
+    // Update is called once per frame
+    public static Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Laser : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float distance;
+
+    public LineRenderer sightLine;
+        
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * UnityEngine.Time.deltaTime);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
+        
+        UnityEngine.Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+        sightLine.SetPosition(1, hitInfo.point);
+                     
+        if (hitInfo.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(2);
+        }
+       
+
+        sightLine.SetPosition(0, transform.position);
+
+    }
+}
